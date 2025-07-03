46f8dd81288fa7855306e396cc11fd15551bb858
Faith Popoola <faith.popoola@cyber.cyberspace.net.ng>
2025-07-03 19:23:54 +0100

    Signup

--- Config/config.py
+++ Config/config.py
@@ -4,7 +4,7 @@ class Config:
     LOGIN_PASSWORD = "nXmNy5J@GmZ6Djv"
     FIRSTNAME = "JOHN"
     LASTNAME = "HASSAN"
-    EMAIL = "john216@sharklasers.com"
+    EMAIL = "jojo11ffhq@sharklasers.com"
     PASSWORD = "Abcd1234#"
     CONTACT_FIRSTNAME = "James"
     CONTACT_LASTNAME = "Bolton"
